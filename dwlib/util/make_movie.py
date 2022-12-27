0b9b40a246fa7d569505cef7dd5802cf6b6cf72f
dowoonlee <dwlee717@gmail.com>
2022-12-27 17:30:12 +0900

    update utils

---
+++ dwlib/util/make_movie.py
@@ -0,0 +1,25 @@
+import imageio
+import glob, os
+
+class make_movie():
+    def __init__(self, path, extension):
+        """
+        path : directory where input images exist
+        extension : extension of images. (jpeg, png, ...)
+        """
+        self._file_list = [f for f in os.listdir(path) if f.endswith(extension)]
+    def to_gif(self, **kwargs):
+        """
+        path : directory to save file
+        output : name of save file
+        fps : fps of gif
+        """
+        path = kwargs["path"]+"/" if "path" in kwargs.keys() else self.path+"/"
+        if not os.path.exists(path):
+            os.mkdir(path)
+        with imageio.get_writer(path + kwargs["output"]+".gif", mode="I", duration=1/kwargs["fps"]) as writer:
+            for filename in self._file_list:
+                filename = os.path.join(path, filename)
+                image = imageio.imread(filename)
+                writer.append_data(image)
+        return
---
+++ util_tutorial.py
@@ -0,0 +1,13 @@
+import numpy as np
+#from dwlib.util.binary_io import Binary_IO
+from dwlib.util.make_movie import make_movie
+import matplotlib.pyplot as plt
+
+# for i in range(10):
+#     x = np.random.rand(10, 10)
+#     plt.imshow(x)
+#     plt.savefig("./test/%02d.png"%(i))
+#     plt.close()
+
+mk = make_movie("./test", "png")
+mk.to_gif(path = "./test", output="output", fps=1)
