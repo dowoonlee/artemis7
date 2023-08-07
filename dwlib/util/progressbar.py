eb797461d7a5e7017a3dd11362d9a4b431aa890f
dwlee717 <dwlee717@gmail.com>
2023-08-07 17:26:54 +0900

    add_HMSnDEL

--- dwlib/util/progressbar.py
+++ dwlib/util/progressbar.py
@@ -1,18 +1,22 @@
 import numpy as np
 import time
 
+
+
+
 class progressbar():
     def __init__(self, maxV, jobname=""):
         self.maxV = maxV
         self._current_percent = 1
         self.t0 = time.time()
-        self.jobname = "."*(max([7-len(jobname), 1]))+jobname
+        self.banner_size = 10
+        self.jobname = "."*(max([self.banner_size-len(jobname), 1]))+jobname
     
     def reset(self, maxV, jobname=""):
         self.maxV = maxV
         self._current_percent = 1
         self.t0 = time.time()
-        self.jobname = "."*(max([7-len(jobname), 1]))+jobname
+        self.jobname = "."*(max([self.banner_size-len(jobname), 1]))+jobname
         
     def start(self):
         print(self._pout(0), end="")
@@ -27,8 +31,8 @@ class progressbar():
         m = eta//60
         s = eta%60
         if h>0:
-            return "ETA %d:%02d"%(h, m)
-        return "ETA %02d:%02d"%(m, s)
+            return "ETA %dH:%02dm"%(h, m)
+        return "ETA %02dm:%02ds"%(m, s)
 
     def _pout(self, percent, eta_disp=True):
         bk = 4-len(str(int(percent)))
@@ -41,14 +45,16 @@ class progressbar():
 
     def update(self, c):
         percent = c/self.maxV*100
-        if np.ceil(percent)>=self._current_percent:
+        if abs(self.maxV-c) <= 1:
+            self.__del__()
+        elif np.ceil(percent)>=self._current_percent:
             print("\r"+self._pout(percent), end="")
-            self._current_percent += 1
-
-    def finish(self):
+            self._current_percent = percent
+    
+    def __del__(self):
         print("\r"+self._pout(100, eta_disp=False), end="")
-        print(" Finish    ")
-        return
+
+
 
 # import time
 # n = 100
