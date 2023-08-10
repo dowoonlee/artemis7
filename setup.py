b82266711cc10612a5b8778a4ad482c69547d131
dwlee717 <dwlee717@gmail.com>
2023-08-10 13:52:35 +0900

    version_update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'dwlib',
-    version='0.0.1',
+    version='0.0.2',
     description='Personal library to compute and draw',
     url ='https://github.com/dowoonlee/dwlib.git',
     author='dwlee',
