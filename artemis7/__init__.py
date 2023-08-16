c6477f9507e7685f5deb7b65f8216d4ad67ef70a
dwlee717 <dwlee717@gmail.com>
2023-08-16 16:47:34 +0900

    update_logo

--- artemis7/__init__.py
+++ artemis7/__init__.py
@@ -1,5 +1,16 @@
-__version__ = "0.0.1"
+__version__ = "0.0.3"
 __all__ = ["artemis7"]
 
-logo = open("logo_ascii").read()
-__doc__ = logo
+__doc__ = "\n".join(
+[" A7 \n", 
+'       d8888 8888888b. 88888888888 8888888888 888b     d888 8888888 .d8888b. 8888888888 ',
+'      d88888 888   Y88b    888     888        8888b   d8888   888  d88P  Y88b      d88P ',
+'     d88P888 888    888    888     888        88888b.d88888   888  Y88b.          d88P  ',
+'    d88P 888 888   d88P    888     8888888    888Y88888P888   888   "Y888b.      d88P   ',
+'   d88P  888 8888888P"     888     888        888 Y888P 888   888      "Y88b. 88888888  ',
+'  d88P   888 888 T88b      888     888        888  Y8P  888   888        "888  d88P     ',
+' d8888888888 888  T88b     888     888        888   "   888   888  Y88b  d88P d88P      ',
+'d88P     888 888   T88b    888     8888888888 888       888 8888888 "Y8888P" d88P       '
+'\n',
+"Analytics, Reporting and Tools for Exploratory Measures in Statistics",
+"Author : DWL \n Contact : arcturus12@yonsei.ac.kr"])
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from artemis7.stats.frechet_inception_distance import FID
 import artemis7 as a7
 import numpy as np
-help(np)
+help(a7)
+
